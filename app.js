455898e44b78c19f12c651cbfcb26e9e7cba001c
Dmitry <ritt@mail.ua>
2016-10-17 22:15:40 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,209 @@
+var app = angular.module('companiesManagerApp', ['ngResource']);
+
+app.factory('Companies', ['$resource', function($resource) {
+    return $resource('/companies', null, {
+        'update': { method: 'PUT' }
+    });
+}]);
+
+app.controller('CompaniesController', ['Companies', function(Companies) {
+    var self = this;
+
+    Companies.prototype.initializeProps = function() {
+        Object.defineProperties(this, {
+            children: {
+                value: [],
+                writable: true
+            },
+
+            parent: {
+                writable: true,
+                configurable: false,
+                enumerable: false
+            },
+
+            totalEarnings: {
+                get: function() {
+                    return this.children.reduce(function(sum, company) {
+                        return sum + company.totalEarnings;
+                    }, +this.earnings);
+                }
+            }
+        });
+    };
+
+    self.newCompany = {};
+    self.allCompanies = [];
+    self.companiesTree = [];
+
+    self.form = {
+        visible: false,
+        submitLabel: 'Add',
+        error: {}
+    };
+
+    Object.defineProperty(self.form, 'canBeSubmitted', {
+        enumerable: true,
+        get: function() {
+            return !Object.keys(this.error).length;
+        }
+    });
+
+    // ---methods---
+
+    self.deleteCompany = function(company, index) {
+        company.$delete({id: company._id}, function(company) {
+            self.removeFromTree(company, index);
+            self.removeFromAll(company);
+        });
+    };
+
+    self.submitForm = function() {
+        if (!self.form.canBeSubmitted) return;
+
+        if (self.form.editingCompany) {
+            var company = self.form.editingCompany;
+
+            company.name = self.newCompany.name;
+            company.earnings = self.newCompany.earnings;
+
+            self.removeFromTree(company);
+
+            if (company.parent) delete company.parentName && (company.parent = null);
+            if (self.newCompany.parent && self.newCompany.type) {
+                company.parent = self.newCompany.parent;
+                company.parentName = company.parent.name;
+            }
+
+            self.addToTree(company);
+            self.addToAll(company);
+
+            delete self.form.editingCompany;
+
+            company.$update({id: company._id}, function(company) {
+            });
+        } else {
+            var company = new Companies(self.newCompany);
+            company.initializeProps();
+
+            delete company.type;
+            if (company.parent) company.parentName = company.parent.name;
+
+            company.$save(function (company) {
+                self.addToTree(company);
+                self.addToAll(company);
+            });
+        }
+
+        self.clearForm();
+    };
+
+    self.startEditing = function(company) {
+        self.newCompany = {};
+        self.newCompany.name = company.name;
+        self.newCompany.earnings = +company.earnings;
+        if (company.parent) (self.newCompany.type = 'sub') && (self.newCompany.parent = company.parent);
+
+        self.form.visible = true;
+        self.form.submitLabel = 'Save';
+        self.form.editingCompany = company;
+
+        self.removeFromAll(company);
+    };
+
+    self.cancelEditing = function() {
+        self.addToAll(self.form.editingCompany);
+
+        delete self.form.editingCompany;
+
+        self.clearForm();
+    };
+
+    self.checkInput = function() {
+        if (self.form.error.nameExists) delete self.form.error.nameExists;
+
+        for (var i = 0; i < self.allCompanies.length; i++) {
+            var company = self.allCompanies[i];
+
+            if (self.newCompany.name === company.name) {
+                self.form.error.nameExists = true;
+                return;
+            }
+        }
+    };
+
+    self.toggleForm = function() {
+        self.form.visible = !self.form.visible;
+    };
+
+    self.clearForm = function() {
+        self.newCompany = {};
+        self.form.visible = false;
+        self.form.submitLabel = 'Add';
+    };
+
+    self.checkType = function() {
+        if (!self.newCompany) return;
+        return self.newCompany.type === 'sub' ||
+            (self.newCompany.parent && (delete self.newCompany.parent && false));
+    };
+
+    self.addToAll = function(company) {
+        self.allCompanies.push(company);
+    };
+
+    self.removeFromAll = function(company) {
+        var index = self.allCompanies.indexOf(company);
+        if (!~index) return;
+        self.allCompanies.splice(index, 1);
+    };
+
+    self.addToTree = function(company) {
+        if (!company.parent) self.companiesTree.push(company);
+        else company.parent.children.push(company);
+    };
+
+    self.removeFromTree = function(company, index) {
+        if (!company.parent) {
+            index = self.companiesTree.indexOf(company);
+            if (!~index) return;
+            self.companiesTree.splice(index, 1);
+        } else {
+            index = company.parent.children.indexOf(company);
+            if (!~index) return;
+            company.parent.children.splice(index, 1);
+        }
+    };
+
+    // ---initial get---
+
+    Companies.query(function(companies) {
+        companies.forEach(function(company) {
+            company.initializeProps();
+        });
+
+        self.allCompanies = companies;
+        self.companiesTree = makeCompaniesTree(companies);
+
+
+        function makeCompaniesTree(companies) {
+            var companiesTree = [];
+
+            companies.forEach(function(company) {
+                if (!company.parentName) return companiesTree.push(company);
+
+                for (var i = 0; i < companies.length; i++) {
+                    var otherCompany = companies[i];
+
+                    if (company.parentName === otherCompany.name) {
+                        otherCompany.children.push(company);
+                        company.parent = otherCompany;
+                        return;
+                    }
+                }
+            });
+
+            return companiesTree;
+        }
+    });
+}]);
---
+++ server.js
@@ -0,0 +1,189 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var mongo = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var urlDB = 'mongodb://localhost:27017/company-manager-db';
+
+var server = http.createServer(function(req, res) {
+    var urlParsed = url.parse(req.url, true);
+
+    switch (urlParsed.pathname) {
+        case '/':
+            sendFile('./index.html', res);
+            break;
+        case '/index.html':
+        case '/app.js':
+            sendFile('.' + urlParsed.pathname, res);
+            break;
+        case '/companies':
+            processRequest(urlParsed.query, req, res);
+            break;
+        default:
+            res.statusCode = 404;
+            res.end('Page Not Found');
+            break;
+    }
+}).listen(8080);
+
+// ---
+
+function sendFile(filePath, res) {
+    var file = fs.createReadStream(filePath);
+    file.pipe(res);
+
+    file.on('error', function() {
+        res.statusCode = 500;
+        res.end('Server Error');
+    });
+
+    res.on('close', function() {
+        file.destroy();
+    });
+}
+
+function processRequest(query, req, res) {
+    mongo.connect(urlDB, function(err, db) {
+        if (err) return res.statusCode = 500 && res.end();
+
+        var companies = db.collection('companies');
+
+        // ---for testing purposes---
+        /*companies.deleteMany({}, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+
+            db.close();
+            res.end();
+        });
+        return;*/
+
+        switch (req.method) {
+            case 'POST':
+                responseToPOST(db, companies, req, res);
+                break;
+            case 'PUT':
+                responseToPUT(db, companies, req, res, query);
+                break;
+            case 'DELETE':
+                responseToDELETE(db, companies, res, query);
+                break;
+            case 'GET':
+                responseToGET(db, companies, res);
+                break;
+        }
+    });
+}
+
+function responseToPOST(db, companies, req, res) {
+    var reqBody = '';
+
+    req.on('data', function(chunk) {
+        reqBody += chunk;
+    });
+
+    req.on('end', function() {
+        var reqBodyParsed = JSON.parse(reqBody);
+
+        companies.insertOne(reqBodyParsed, function(err, result) {
+            db.close();
+            if (err) return res.statusCode = 500 && res.end();
+
+            console.log('New doc was successfully added to collection');
+            reqBodyParsed._id = result.insertedId;
+            var resBody = JSON.stringify(reqBodyParsed);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.end(resBody);
+        });
+    });
+}
+
+function responseToPUT(db, companies, req, res, query) {
+    var reqBody = '';
+
+    req.on('data', function(chunk) {
+        reqBody += chunk;
+    });
+
+    req.on('end', function() {
+        var reqBodyParsed = JSON.parse(reqBody);
+        delete reqBodyParsed._id;
+
+        companies.updateOne(
+            { _id: ObjectId(query.id) },
+            { $set: reqBodyParsed }, function(err, result) {
+                db.close();
+                if (err) return res.statusCode = 500 && res.end();
+
+                console.log('Doc was successfully updated in collection');
+                res.end();
+            }
+        );
+    });
+}
+
+function responseToDELETE(db, companies, res, query) {
+    companies.deleteOne({ _id: ObjectId(query.id) }, function(err, result) {
+        db.close();
+        if (err) return res.statusCode = 500 && res.end();
+
+        console.log('Doc was successfully removed from collection');
+        res.end();
+    });
+}
+
+function responseToGET(db, companies, res) {
+    companies.count(function(err, count) {
+        // if there is no collection -
+        //      create it, set index and fill with init. data from json file
+        if (!count) {
+            companies.createIndex({ 'name': 1 }, { unique: true }, function(err, result) {
+                var file = fs.createReadStream('./companies.json');
+                var json = '';
+
+                file.on('readable', function(data) {
+                    var chunk = file.read();
+                    if (!chunk) return;
+                    json += chunk;
+                });
+
+                file.on('end', function() {
+                    var jsonParsed = JSON.parse(json);
+
+                    companies.insertMany(jsonParsed, function(err, result) {
+                        if (err) console.log('Something wrong with insert docs from file');
+
+                        console.log('New docs (from json file) were successfully added to collection');
+                        returnAllDocuments();
+                    });
+                });
+
+                file.on('error', function() {
+                    res.statusCode = 500;
+                    res.end('Server Error');
+                });
+
+                res.on('close', function() {
+                    file.destroy();
+                });
+            });
+        } else {
+            returnAllDocuments();
+        }
+    });
+
+    // ---
+    function returnAllDocuments() {
+        companies.find().toArray(function(err, documents) {
+            db.close();
+            if (err) return res.statusCode = 500 && res.end();
+
+            var resBody = JSON.stringify(documents);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.end(resBody);
+        });
+    }
+}
